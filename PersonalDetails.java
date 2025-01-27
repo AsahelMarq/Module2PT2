e2239ede3d67041912e153197217394bdbedf662
AsahelMarq <amSalangsang@mcm.edu.ph>
2025-01-27 13:44:53 +0800

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,19 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Asahel Marq Salangsang";
+        int age = 18;
+        double height = 1.62;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+
+        System.out.println("Hi, my name is " + name + ". I am " + age + " years old, " + height + " meters tall, and it is " + isStudent + " that I am a student.");
+    }
+    
+}
+
+
+
